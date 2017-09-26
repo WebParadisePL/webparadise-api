91a0693ed2c7c11c57b2ac766931188d8b401845
WebParadise Pvt. Ltd <business@webparadise.ml>
2017-09-26 15:08:11 +0545

    Edited

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const service = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var service = express();
 
 service.use(bodyParser.json());
+service.use(bodyParser.urlencoded({extended: true}));
 
-service.post("/ios", function(req, res) {
-	if (req.body.actionType == "ml.webparadise.api.action.OBJC_ACTION") {
-		return res.json({
-			importModule: "from objc_util import *\n"
-		});
-	}
-	if (req.body.function == "ml.webparadise.api.function.SET_BRIGHTNESS") {
-		return res.json({
-			codeBody: "UIScreen = ObjCClass('UIScreen')\n screen = UIScreen.mainScreen()\n screen.setBrightness(1.0)"
-		});
+service.post("/pythonista", function(req, res) {
+	res.setHeader("Content-Type", "application/json");
+	
+	var brightnessValue = req.body.brightnessValue;
+	
+	if (req.body.actionType == "ml.webparadise.api.action.ObjCAction" && req.body.function == "ml.webparadise.api.function.setBrightness") {
+		res.send(JSON.stringify({
+			importModule: "from objc_util import *\n",
+			codeBody: "UIScreen = ObjCClass('UIScreen')\n screen = UIScreen.mainScreen()\n screen.setBrightness_(" + brightnessValue + ")\n"
+		}));
 	}
 });
 

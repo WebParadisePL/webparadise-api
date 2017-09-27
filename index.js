1b39d24079d5ea6114acb6e7dbcd15741823ac9b
WebParadise Pvt. Ltd <business@webparadise.ml>
2017-09-27 08:02:57 +0545

    Edit

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ service.use(bodyParser.json());
 service.use(bodyParser.urlencoded({extended: true}));
 service.set('port', (process.env.PORT || 3000));
 
-service.get('/pythonista', function(request, response) {
+service.post('/pythonista', function(request, response) {
 	response.setHeader('Content-Type', 'application/json');
 	
 	var brightnessValue = request.body.brightnessValue;
 	
-	if (request.body.actionType == 'ml.webparadise.api.action.ObjCAction' && request.body.functionType == 'ml.webparadise.api.function.setBrightness') {
-		response.send(JSON.stringify({
-			importModule: 'from objc_util import *\n',
-			codeBody: 'UIScreen = ObjCClass("UIScreen")\n screen = UIScreen.mainScreen()\n screen.setBrightness_(' + brightnessValue + ')\n'
-		}));
-	}
+	setTimeout(function() {
+		if (request.body.actionType == 'ml.webparadise.api.action.ObjCAction' && request.body.functionType == 'ml.webparadise.api.function.setBrightness') {
+			response.send(JSON.stringify({
+				importModule: 'from objc_util import *\n',
+				codeBody: 'UIScreen = ObjCClass("UIScreen")\n screen = UIScreen.mainScreen()\n screen.setBrightness_(' + brightnessValue + ')\n'
+			}));
+		}
+	}, 1000)
 });
 
 service.listen(service.get('port'), function() {

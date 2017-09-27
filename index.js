2fd2681dae73c353890ac779bb8ad30e93663783
WebParadise Pvt. Ltd <business@webparadise.ml>
2017-09-27 19:54:23 +0545

    Edit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const service = express();
 
 service.use(bodyParser.json());
 service.use(bodyParser.urlencoded({extended: true}));
+
 service.set('port', (process.env.PORT || 3000));
 
 service.post('/pythonista', function(request, response) {
@@ -22,6 +22,36 @@ service.post('/pythonista', function(request, response) {
 			}));
 		}
 	}, 1000)
+});
+
+service.post('/oauth/python/v1.1/authorize', function(request, response) {
+	response.setHeader('Content-Type', 'application/json');
+	
+	if (request.body.username == "user_admin" && request.body.password == "passkey123") {
+		response.send(JSON.stringify({
+			authCallbackCode: 200,
+			giveAccess = true,
+			message: "You are welcome to Simple Auth."
+		}));
+	} else if (request.body.username !== "user_admin" && request.body.password == "passkey123") {
+		response.send(JSON.stringify({
+			authCallbackCode: 201,
+			giveAccess = false,
+			message: "Your username is incorrect."
+		}));
+	} else if (request.body.username == "user_admin" && request.body.password !== "passkey123") {
+		response.send(JSON.stringify({
+			authCallbackCode: 202,
+			giveAccess = false,
+			message: "Your password is incorrect."
+		}));
+	} else {
+		response.send(JSON.stringify({
+			authCallbackCode: 203,
+			giveAccess = false,
+			message: "Your username & password are incorrect."
+		}));
+	}
 });
 
 service.listen(service.get('port'), function() {
